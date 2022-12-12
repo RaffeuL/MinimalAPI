0d2a6de43bd1759df45b4d53e5bdeaef3adb0ada
unknown <rafael91.lisboa@hotmail.com>
2022-12-12 13:18:39 -0300

    Mini Validation

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI.Data;
 using MinimalAPI.Models;
+using MiniValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,29 +21,42 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/fornecedor", async (MinimalContentxDb context) =>
-    await context.Forncedores.ToListAsync())
-    .WithName("GetFornecedores")
-    .WithTags("Fornecedor");
+
+app.MapGet("/fornecedores", async (MinimalContentxDb context) =>
+    await context.Forncedores.ToListAsync()
+)
+.WithName("GetFornecedores")
+.WithTags("Fornecedor");
+
 
 app.MapGet("/fornecedor/{id}", async (Guid id, MinimalContentxDb context) =>
     await context.Forncedores.FindAsync(id)
-        is Fornecedor fornecedor ? Results.Ok(fornecedor) : Results.NotFound())
-    .Produces<Fornecedor>(StatusCodes.Status200OK)
-    .Produces(StatusCodes.Status404NotFound)
-    .WithName("GetFornecedoresPorId")
-    .WithTags("Fornecedor");
+        is Fornecedor fornecedor ? Results.Ok(fornecedor) : Results.NotFound()
+)
+.Produces<Fornecedor>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound)
+.WithName("GetFornecedoresPorId")
+.WithTags("Fornecedor");
 
-app.MapPost("/fornecedor/", async (MinimalContentxDb context, Fornecedor fornecedor) =>
+
+app.MapPost("/fornecedor/", async (MinimalContentxDb context, Fornecedor fornecedor) => 
 {
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
     context.Forncedores.Add(fornecedor);
     var result = await context.SaveChangesAsync();
 
+    return result > 0
+        //? Results.Created($"/fornecedor/{fornecedor.Id}", fornecedor })
+        ? Results.CreatedAtRoute("GetFornecedorPorId", new { id = fornecedor.Id, fornecedor })
+        : Results.BadRequest("Houve um problemaao salvar o registro");
+
 })
-    .Produces<Fornecedor>(StatusCodes.Status201Created)
-    .Produces(StatusCodes.Status400BadRequest)
-    .WithName("PostFornecedores")
-    .WithTags("Fornecedor");
+.Produces<Fornecedor>(StatusCodes.Status201Created)
+.Produces(StatusCodes.Status400BadRequest)
+.WithName("PostFornecedores")
+.WithTags("Fornecedor");
    
 
 app.Run();
